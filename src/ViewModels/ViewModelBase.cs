d1ef237e7a45222fc183953e735c483ffd117958
Lee ALexander <lee@binarycomponents.com>
2021-10-31 15:04:42 +0000

    Moved files around

---
+++ src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QxLaunch.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
