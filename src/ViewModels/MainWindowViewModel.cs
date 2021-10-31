6cc25380a5320e446b9479903b7371bb0fc12f52
Lee ALexander <lee@binarycomponents.com>
2021-10-31 16:32:43 +0000

    Initial frameout  of classes

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,9 @@ namespace QxLaunch.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public void ShowOptions()
+        {
+            Console.WriteLine("");
+        }
     }
 }
